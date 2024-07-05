54462720b58dd74bff8a0aff9b1ada8d7f3f2eb0
SAlmonte <almontesauel@gmail.com>
2024-07-04 23:07:26 -0400

    Updating Portfolio

--- src/components/UnderCunstruction.js
+++ src/components/UnderCunstruction.js
@@ -2,12 +2,19 @@ import React from "react";
 
 function UnderConstruction() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-			<h1 className="text-4xl font-bold text-gray-800">Under Construction</h1>
-			<p className="mt-4 text-xl text-gray-600">
-				We are working hard to bring you a new and improved portfolio. Stay
-				tuned!
-			</p>
+		<div className="relative flex items-center justify-center min-h-screen bg-gray-100">
+			<img
+				src="../assets/cunstruction/under_construction-1.jpg"
+				alt="Under Construction"
+				className="absolute inset-0 object-cover w-full h-full"
+			/>
+			<div className="relative z-10 p-4 bg-black bg-opacity-50 rounded">
+				<h1 className="text-4xl font-bold text-white">Under Construction</h1>
+				<p className="mt-4 text-xl text-white">
+					I am working hard to bring you a new and improved portfolio. Stay
+					tuned!
+				</p>
+			</div>
 		</div>
 	);
 }
