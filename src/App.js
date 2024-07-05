214b95b38400b942763a09f9a4f094a1d230c9ef
SAlmonte <almontesauel@gmail.com>
2024-07-04 23:02:27 -0400

    Updating Portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
-import Work from "./components/Work";
+// import About from "./components/About";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+// import Home from "./components/Home";
+// import Navbar from "./components/Navbar";
+// import Skills from "./components/Skills";
+// import Work from "./components/Work";
 import UnderConstruction from "./components/UnderCunstruction";
 
 function App() {
